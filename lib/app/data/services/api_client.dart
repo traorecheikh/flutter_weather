9d5e6ea8f9ac39e15f311f6e692de0279c4e5773
traorecheikh <atidiane741@gmail.com>
2025-03-11 19:16:43 +0000

    test

--- lib/app/data/services/api_client.dart
+++ lib/app/data/services/api_client.dart
@@ -1,4 +1,3 @@
-// lib/core/services/weather_api_client.dart
 import 'package:weatherapi/weatherapi.dart';
 import 'package:weatherinx/app/core/constants/api_constants.dart';
 import 'package:weatherinx/app/data/services/exceptions.dart';
@@ -6,69 +5,103 @@ import 'package:weatherinx/app/data/services/exceptions.dart';
 class WeatherApiClient {
   final WeatherRequest _weatherRequest;
 
+  // Constructeur qui initialise l'objet WeatherRequest avec la cle API et la langue
   WeatherApiClient()
-    : _weatherRequest = WeatherRequest(ApiConstants.apiKey,language: Language.french);
+    : _weatherRequest = WeatherRequest(
+        ApiConstants.apiKey,
+        language: Language.french,
+      );
 
+  // Methode pour obtenir la meteo en temps reel d'une ville
   Future<RealtimeWeather> getRealtimeWeatherByCity(String cityName) async {
     try {
+      // Appel de l'API pour obtenir la meteo en temps reel d'une ville
       return await _weatherRequest.getRealtimeWeatherByCityName(cityName);
     } catch (error) {
-      throw WeatherApiException('Failed to fetch realtime weather: $error');
+      // En cas d'erreur, on lance une exception avec le message d'erreur
+      throw WeatherApiException(
+        'Echec de la recuperation de la meteo en temps reel: $error',
+      );
     }
   }
 
+  // Methode pour obtenir la meteo en temps reel par localisation (latitude et longitude)
   Future<RealtimeWeather> getRealtimeWeatherByLocation(
     double latitude,
     double longitude,
   ) async {
     try {
+      // Appel de l'API pour obtenir la meteo en temps reel par localisation
       return await _weatherRequest.getRealtimeWeatherByLocation(
         latitude,
         longitude,
       );
     } catch (error) {
-      throw WeatherApiException('Failed to fetch realtime weather: $error');
+      // En cas d'erreur, on lance une exception avec le message d'erreur
+      throw WeatherApiException(
+        'Echec de la recuperation de la meteo en temps reel: $error',
+      );
     }
   }
 
+  // Methode pour obtenir les previsions meteo d'une ville
   Future<ForecastWeather> getForecastWeatherByCity(String cityName) async {
     try {
+      // Appel de l'API pour obtenir les previsions meteo d'une ville
       return await _weatherRequest.getForecastWeatherByCityName(cityName);
     } catch (error) {
-      throw WeatherApiException('Failed to fetch forecast weather: $error');
+      // En cas d'erreur, on lance une exception avec le message d'erreur
+      throw WeatherApiException(
+        'Echec de la recuperation des previsions meteo: $error',
+      );
     }
   }
 
+  // Methode pour obtenir les previsions meteo par localisation (latitude et longitude)
   Future<ForecastWeather> getForecastWeatherByLocation(
     double latitude,
     double longitude,
   ) async {
     try {
+      // Appel de l'API pour obtenir les previsions meteo par localisation
       return await _weatherRequest.getForecastWeatherByLocation(
         latitude,
         longitude,
       );
     } catch (error) {
-      throw WeatherApiException('Failed to fetch forecast weather: $error');
+      // En cas d'erreur, on lance une exception avec le message d'erreur
+      throw WeatherApiException(
+        'Echec de la recuperation des previsions meteo: $error',
+      );
     }
   }
 
+  // Methode pour rechercher des localisations par nom de ville
   Future<SearchResults> searchLocationsByCity(String cityName) async {
     try {
+      // Appel de l'API pour rechercher des localisations par nom de ville
       return await _weatherRequest.getResultsByCityName(cityName);
     } catch (error) {
-      throw WeatherApiException('Failed to search locations: $error');
+      // En cas d'erreur, on lance une exception avec le message d'erreur
+      throw WeatherApiException(
+        'Echec de la recherche des localisations: $error',
+      );
     }
   }
 
+  // Methode pour rechercher des localisations par coordonnees (latitude et longitude)
   Future<SearchResults> searchLocationsByCoordinates(
     double latitude,
     double longitude,
   ) async {
     try {
+      // Appel de l'API pour rechercher des localisations par coordonnees
       return await _weatherRequest.getResultsByLocation(latitude, longitude);
     } catch (error) {
-      throw WeatherApiException('Failed to search locations: $error');
+      // En cas d'erreur, on lance une exception avec le message d'erreur
+      throw WeatherApiException(
+        'Echec de la recherche des localisations: $error',
+      );
     }
   }
 }
